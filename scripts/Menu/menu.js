9140e131c856ac764e53fcbe9ead51badc0c829f
João Filipe <126769503+FilipeDiass@users.noreply.github.com>
2023-10-05 15:41:53 -0300

    att

--- scripts/Menu/menu.js
+++ scripts/Menu/menu.js
@@ -1,3 +1,4 @@
+// Abrir/Fechar Menu Burguer
 const menuBurgue = document.querySelector('#menuBurgue')
 function Menu(event){
     event.preventDefault()
@@ -9,9 +10,32 @@ function Menu(event){
     const chaveMenu = campMobile.classList.contains('ativo')
     if(chaveMenu === true){
         campMobile.style.visibility = 'visible'
+        menuBurgue.style.zIndex = '10'
     }else{
         campMobile.style.visibility = 'hidden'
+        menuBurgue.style.zIndex = '0'
     }
 }
 menuBurgue.addEventListener('click', Menu)
 menuBurgue.addEventListener('touchstart', Menu)
+
+// Fechar/Abrir Menu Carrinho
+const fecharCart = document.querySelector('#fecharCart')
+const abrirCart = document.querySelector('#abrirCart')
+function menuCarrinho(event){
+    event.preventDefault()
+
+    const containerCart = document.querySelector('#containerCart')
+    containerCart.classList.toggle('active')
+
+    const chaveMenu = containerCart.classList.contains('active')
+    if(chaveMenu === true){
+        containerCart.style.visibility = 'visible'
+    }else{
+        containerCart.style.visibility = 'hidden'
+    }
+}
+fecharCart.addEventListener('click', menuCarrinho)
+fecharCart.addEventListener('touchstart', menuCarrinho)
+abrirCart.addEventListener('click', menuCarrinho)
+abrirCart.addEventListener('touchstart', menuCarrinho)
