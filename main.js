b30d2da7688ea1bfb53299b1abf48edb2e7531ad
João Filipe <126769503+FilipeDiass@users.noreply.github.com>
2023-10-06 15:16:26 -0300

    att big menu

---
+++ main.js
@@ -0,0 +1,4 @@
+import { iniciarMenu } from './scripts/Menu/menu.js';
+
+//Habilita as funções do Menu/Header
+iniciarMenu();
--- scripts/Menu/menu.js
+++ scripts/Menu/menu.js
@@ -1,7 +1,7 @@
 // Abrir/Fechar Menu Burguer
-const menuBurgue = document.querySelector('#menuBurgue')
 function Menu(event){
     event.preventDefault()
+
     const campMobile = document.querySelector('#menuMobile')
     const campIcones = document.querySelector('#campIcones')
     campMobile.classList.toggle('ativo')
@@ -16,18 +16,15 @@ function Menu(event){
         menuBurgue.style.zIndex = '0'
     }
 }
-menuBurgue.addEventListener('click', Menu)
-menuBurgue.addEventListener('touchstart', Menu)
 
-// Fechar/Abrir Menu Carrinho
-const fecharCart = document.querySelector('#fecharCart')
-const abrirCart = document.querySelector('#abrirCart')
+// Abrir/Fechar Menu Carrrinho
 function menuCarrinho(event){
     event.preventDefault()
 
     const containerCart = document.querySelector('#containerCart')
-    containerCart.classList.toggle('active')
 
+    containerCart.classList.toggle('active')
+    
     const chaveMenu = containerCart.classList.contains('active')
     if(chaveMenu === true){
         containerCart.style.visibility = 'visible'
@@ -35,7 +32,60 @@ function menuCarrinho(event){
         containerCart.style.visibility = 'hidden'
     }
 }
-fecharCart.addEventListener('click', menuCarrinho)
-fecharCart.addEventListener('touchstart', menuCarrinho)
-abrirCart.addEventListener('click', menuCarrinho)
-abrirCart.addEventListener('touchstart', menuCarrinho)
+
+// Abrir/Fechar Menu Favorito
+function menuFavorito(event){
+    event.preventDefault()
+
+    const containerFavo = document.querySelector('#containerFavo')
+
+    containerFavo.classList.toggle('active')
+    
+    const chaveMenu = containerFavo.classList.contains('active')
+    if(chaveMenu === true){
+        containerFavo.style.visibility = 'visible'
+    }else{
+        containerFavo.style.visibility = 'hidden'
+    }
+}
+
+export function iniciarMenu(){
+    // Inicializar Abrir/Fechar Menu Burguer
+    const menuBurgue = document.querySelector('#menuBurgue')
+    menuBurgue.addEventListener('click', Menu)
+    menuBurgue.addEventListener('touchstart', Menu)
+
+    // Inicializar Abrir Menu Cart
+    const abrirCart = document.querySelector('#abrirCart')
+    abrirCart.addEventListener('click', menuCarrinho)
+    abrirCart.addEventListener('touchstart', menuCarrinho)
+
+    // Inicializar Fechar Menu Cart
+    const fecharCart = document.querySelector('#fecharCart')
+    const voltaLoja = document.querySelector('#voltaLoja')
+    fecharCart.addEventListener('click', menuCarrinho)
+    fecharCart.addEventListener('touchstart', menuCarrinho)
+    voltaLoja.addEventListener('click', menuCarrinho)
+    voltaLoja.addEventListener('touchstart', menuCarrinho)
+
+    // Inicializar Fechar Menu Favo
+    const fecharFavo = document.querySelector('#fecharFavo')
+    const voltaLojaFavo = document.querySelector('#voltaLojaFavo')
+    fecharFavo.addEventListener('click', menuFavorito)
+    fecharFavo.addEventListener('touchstart', menuFavorito)
+    voltaLojaFavo.addEventListener('click', menuFavorito)
+    voltaLojaFavo.addEventListener('touchstart', menuFavorito)
+
+    // Inicializar Opção Menu Burguer
+    const meusPedidos = document.querySelector('#meusPedidos')
+    meusPedidos.addEventListener('click', Menu)
+    meusPedidos.addEventListener('touchstart', Menu)
+    meusPedidos.addEventListener('click', menuCarrinho)
+    meusPedidos.addEventListener('touchstart', menuCarrinho)
+
+    const meusFavoritos = document.querySelector('#meusFavoritos')
+    meusFavoritos.addEventListener('click', Menu)
+    meusFavoritos.addEventListener('touchstart', Menu)
+    meusFavoritos.addEventListener('click', menuFavorito)
+    meusFavoritos.addEventListener('touchstart', menuFavorito)
+}
