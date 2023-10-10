63fc7f24b9f82e8f2451fde4b48862c0b99eef89
João Filipe <126769503+FilipeDiass@users.noreply.github.com>
2023-10-10 16:02:30 -0300

    att

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 import { iniciarMenu } from './scripts/Menu/menu.js';
+import iniciaSlideBanner from './scripts/Slide/slideBanner.js';
 
 //Habilita as funções do Menu/Header
 iniciarMenu();
+
+//Habilita as funções do slideBanner
+iniciaSlideBanner()
--- scripts/Slide/slideBanner.js
+++ scripts/Slide/slideBanner.js
@@ -1,10 +1,25 @@
-const pointer1 = document.querySelector('#pointer1')
-const pointer2 = document.querySelector('#pointer2')
 function slideBanner(event){
+    event.preventDefault()
     const marginBanner = document.querySelector('#marginSlide')
 
-    marginBanner.classList.toggle('passar')
-
+    if(pointer1 === event.currentTarget){
+        marginBanner.classList.remove('passar')
+        pointer1.style.backgroundColor = 'white'
+        pointer2.style.backgroundColor = 'initial'
+    }else{
+        marginBanner.classList.add('passar')
+        pointer1.style.backgroundColor = 'initial'
+        pointer2.style.backgroundColor = 'white'
+    }
 }
-pointer1.addEventListener('click', slideBanner)
-pointer2.addEventListener('click', slideBanner)
+
+
+export default function iniciaSlideBanner(){
+    const pointer1 = document.querySelector('#pointer1')
+    const pointer2 = document.querySelector('#pointer2')
+
+    pointer1.addEventListener('click', slideBanner)
+    pointer2.addEventListener('click', slideBanner)
+    pointer1.addEventListener('touchstart', slideBanner)
+    pointer2.addEventListener('touchstart', slideBanner)
+}
