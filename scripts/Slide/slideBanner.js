48f8cea3d95110f03cbdafe90ed44a62c00828e9
João Filipe <126769503+FilipeDiass@users.noreply.github.com>
2023-10-11 16:42:12 -0300

    att

--- scripts/Slide/slideBanner.js
+++ scripts/Slide/slideBanner.js
@@ -1,25 +1,29 @@
 function slideBanner(event){
-    event.preventDefault()
-    const marginBanner = document.querySelector('#marginSlide')
-
-    if(pointer1 === event.currentTarget){
-        marginBanner.classList.remove('passar')
+    // event.preventDefault();
+    if(event.pointerType === 'touch') return;
+    
+    console.log(event)
+    if(pointer1 === event.currentTarget || (imgSlide.scrollLeft < 100 && imgSlide.scrollLeft > 0) ){
+        imgSlide.scrollLeft = 0
         pointer1.style.backgroundColor = 'white'
         pointer2.style.backgroundColor = 'initial'
-    }else{
-        marginBanner.classList.add('passar')
+    }else if(pointer2 === event.currentTarget || imgSlide.scrollLeft >= 100){
+        imgSlide.scrollLeft = 700
         pointer1.style.backgroundColor = 'initial'
         pointer2.style.backgroundColor = 'white'
     }
 }
 
 
+
 export default function iniciaSlideBanner(){
     const pointer1 = document.querySelector('#pointer1')
     const pointer2 = document.querySelector('#pointer2')
+    const imgSlide = document.querySelector('#imgSlide')
 
     pointer1.addEventListener('click', slideBanner)
     pointer2.addEventListener('click', slideBanner)
     pointer1.addEventListener('touchstart', slideBanner)
     pointer2.addEventListener('touchstart', slideBanner)
+    imgSlide.addEventListener('touchend', slideBanner)
 }
