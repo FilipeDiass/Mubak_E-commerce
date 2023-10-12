72017ea0a2ae51e55d550fffa727520325abf491
João Filipe <126769503+FilipeDiass@users.noreply.github.com>
2023-10-12 15:54:37 -0300

    att

--- main.js
+++ main.js
@@ -6,3 +6,7 @@ iniciarMenu();
 
 //Habilita as funções do slideBanner
 iniciaSlideBanner()
+
+
+
+
--- scripts/Slide/slideBanner.js
+++ scripts/Slide/slideBanner.js
@@ -1,29 +1,20 @@
-function slideBanner(event){
-    // event.preventDefault();
-    if(event.pointerType === 'touch') return;
-    
-    console.log(event)
-    if(pointer1 === event.currentTarget || (imgSlide.scrollLeft < 100 && imgSlide.scrollLeft > 0) ){
-        imgSlide.scrollLeft = 0
-        pointer1.style.backgroundColor = 'white'
-        pointer2.style.backgroundColor = 'initial'
-    }else if(pointer2 === event.currentTarget || imgSlide.scrollLeft >= 100){
-        imgSlide.scrollLeft = 700
-        pointer1.style.backgroundColor = 'initial'
-        pointer2.style.backgroundColor = 'white'
-    }
-}
-
-
-
 export default function iniciaSlideBanner(){
-    const pointer1 = document.querySelector('#pointer1')
-    const pointer2 = document.querySelector('#pointer2')
-    const imgSlide = document.querySelector('#imgSlide')
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
 
-    pointer1.addEventListener('click', slideBanner)
-    pointer2.addEventListener('click', slideBanner)
-    pointer1.addEventListener('touchstart', slideBanner)
-    pointer2.addEventListener('touchstart', slideBanner)
-    imgSlide.addEventListener('touchend', slideBanner)
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
 }
+
