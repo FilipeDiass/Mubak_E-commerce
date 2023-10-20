1d667d7f6b5a195c6cb4eb303c628fb6e8152a81
João Filipe <126769503+FilipeDiass@users.noreply.github.com>
2023-10-20 14:54:00 -0300

    att

--- scripts/Cards/insereCards.js
+++ scripts/Cards/insereCards.js
@@ -1,20 +1,33 @@
-import pegaDados from "./produto.js";
+import pegaDados from "./pegaDados.js";
 import criaCorpoCard from "./card.js";
 
 export async function criaCardsDestaque() { 
     const data = await pegaDados();
     const produto = data.itensDestaque
-    
+
+    embaralhaArray(produto)
     //Adiciona Cards na aba Destaques
     const containerCard = document.querySelector('.cardDestaque')
     produto.forEach((prod)=>{
         const cards = criaCorpoCard(prod)
+        cards.id = prod.id
         containerCard.appendChild(cards)  
     })
+    console.log(containerCard)
 }
 
 export async function criaCardsEstoque(){
     // const data = await pegaDados();
     // const produto = data.itensEstoque
     // console.log(produto)
+}
+
+// Função para embaralhar um array
+function embaralhaArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        // Número aleatório
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
