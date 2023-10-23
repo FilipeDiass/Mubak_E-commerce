4a4b5eb438b3323753747c55c54775ecd18bb46c
João Filipe <126769503+FilipeDiass@users.noreply.github.com>
2023-10-23 13:33:15 -0300

    att

--- scripts/Cards/scroolCard.js
+++ scripts/Cards/scroolCard.js
@@ -1,7 +1,24 @@
 const circleLeft = document.querySelector('.circleLeft')
 const circleRight = document.querySelector('.circleRigth')
 export default function scrollCard(){
-    console.log('Show!')
+    const campCards = document.querySelector('.campCards')
+    function scroll(event){
+        if(event.currentTarget === circleRight){
+            campCards.scrollLeft += 800
+            event.currentTarget.removeEventListener('click', scroll)
+        }else if(event.currentTarget === circleLeft){
+            campCards.scrollLeft -= 800
+            event.currentTarget.removeEventListener('click', scroll)
+        }
+        
+        setTimeout(()=>{
+            circleRight.addEventListener('click', scroll)
+            circleLeft.addEventListener('click', scroll)
+        }, 550)
+    }
+    circleLeft.addEventListener('click', scroll)
+    circleRight.addEventListener('click', scroll)
 }
-circleLeft.addEventListener('click', scrollCard)
-circleRight.addEventListener('click', scrollCard)
+
+
+
